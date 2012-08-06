1df90c4d58f81021eaf416ffc92b967ddd805a37
unknown <kdelacruz@KD.(none)>
2012-08-05 21:08:07 -0500

    Commit numero 14 - Romano 15

--- features/step_definitions/roman_steps.rb
+++ features/step_definitions/roman_steps.rb
@@ -16,57 +16,11 @@ end
 
 
 class Conversor
-	def convertir_romano(numero)
-		return "I" if numero == 1 		
-		return dame_romano_2_5(numero)	if numero < 6	
-		return dame_romano_6_10(numero)	if numero < 11	
-		return dame_romano_11_15(numero)	if numero < 16	
-    end
 
-
-    def dame_romano_2_5(numero)
-	    case numero  
-	        when 2  
-	        	 then "II"
-	        when 3  
-	        	 then "III"
-	       	when 4
-	        	 then "IV"
-	    	else  "V"        
-	    end
-	end
-
-	def dame_romano_6_10(numero)
-		w = { 	6 => "VI", 
-				7 => "VII",
-				8 => "VIII",
-				9 => "IX",
-				10 => "X" }
-		return w.fetch( numero ) if w.has_key?( numero )
-	end 	
-
-
-	def dame_romano_11_15(numero)
-
-		count = 0		
-		num_romano = ""		
-		simb = ["X", "IX", "V", "IV", "I"]
-		valores_num = [10, 9, 5, 4, 1 ]  
-
-		while numero > 0 do
-			if numero < valores_num[count]
-				count = count + 1
-			else
-				num_romano = num_romano + simb[count]
-				numero = numero - valores_num[count]
-			end
-		end
-		return num_romano
-	end
 
 	def dame_romano_16_20(numero)
 
-
+		if numero
 
 	end
 
